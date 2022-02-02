041d0af87b0875210dac57728e64f078e36b03e4
Axel Olivas <axelbrolivas@gmail.com>
2022-02-02 10:51:20 -0800

    second commit

--- js-file.js
+++ js-file.js
@@ -8,6 +8,14 @@ let blueH3 = document.createElement('h3')
 blueH3.style.color = 'blue'
 blueH3.textContent = "I'm a blue H3!"
 
+let blackPink = document.createElement('div')
+blackPink.setAttribute('style', 'border: 2px solid black; background-color: pink;')
 
+let h1 = document.createElement('h1')
+h1.textContent = "I'm in a div"
 
-div.append(redP, blueH3)
+let p = document.createElement('p')
+p.textContent = "ME TOO!"
+
+blackPink.append(h1, p)
+div.append(redP, blueH3, blackPink)
